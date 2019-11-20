1919c0ddd5737b224d1034d1dd5e2050f140fd7c
Chuhan Jie <jiechuhan1996@gmail.com>
2019-11-20 11:31:16 -0800

    image API

--- routes/api/image.js
+++ routes/api/image.js
@@ -1,17 +1,62 @@
 const router = require("express").Router();
-const postsController = require("../../controllers/postsController");
+const db = require("../../models/images")
+// const postsController = require("../../controllers/postsController");
 
-// // Matches with "/api/posts"
-// router
-//   .route("/")
-//   .get(postsController.findAll)
-//   .post(postsController.create);
+// Matches with "/api/posts"
+router
+  .route("/")
+  .get(
+    function(req, res) {
+          db.Post.find(req.query)
+            .sort({ date: -1 })
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    })
+  .post(postsController.create);
 
-// // Matches with "/api/posts/:id"
-// router
-//   .route("/:id")
-//   .get(postsController.findById)
-//   .put(postsController.update)
-//   .delete(postsController.remove);
+// Matches with "/api/posts/:id"
+router
+  .route("/:id")
+  .get(postsController.findById)
+  .put(postsController.update)
+  .delete(postsController.remove);
+
+
+
+app.post(/api/images) // add images to database
+
+app.get(/api/images?start=10&count=10) // get food type associate with img
+
+
+// findAll: function(req, res) {
+  //   db.Post.find(req.query)
+  //     .sort({ date: -1 })
+  //     .then(dbModel => res.json(dbModel))
+  //     .catch(err => res.status(422).json(err));
+  // },
+  // findById: function(req, res) {
+  //   db.Post.findById(req.params.id)
+  //     .then(dbModel => res.json(dbModel))
+  //     .catch(err => res.status(422).json(err));
+  // },
+  // create: function(req, res) {
+  //   db.Post.create(req.body)
+  //     .then(dbModel => res.json(dbModel))
+  //     .catch(err => res.status(422).json(err));
+  // },
+  // update: function(req, res) {
+  //   db.Post.findOneAndUpdate({ _id: req.params.id }, req.body)
+  //     .then(dbModel => res.json(dbModel))
+  //     .catch(err => res.status(422).json(err));
+  // },
+  // remove: function(req, res) {
+  //   db.Post.findById({ _id: req.params.id })
+  //     .then(dbModel => dbModel.remove())
+  //     .then(dbModel => res.json(dbModel))
+  //     .catch(err => res.status(422).json(err));
+  // }
+
+
+
 
 module.exports = router;
