9fb60ef6ac958f89168741d470ddf56b26eb0d66
Claire Romillo <c_romillo@yahoo.com>
2019-11-21 12:05:15 -0800

    added username & password input on login And styling page

--- client/src/pages/LogIn.js
+++ client/src/pages/LogIn.js
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { Component } from 'react';
+import loginImg from "../photos/feedmePhoto.jpg";
+import "./LogIn.css"
 
-const LogIn = () => {
-  // return (
-    
-  // );
-};
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-export default LogIn;
+  render() {
+    return (
+      <div className="base-container" ref={this.props.containerRef}>
+        <div className="header">Login</div>
+        <div className="content">
+          <div className="image">
+            <img className="styleMe" src={loginImg} alt="restaurantPhoto"/>
+          </div>
+          <div className="form">
+            <div className="form-group">
+              <label htmlFor="username">Username</label>
+              <input type="text" name="username" placeholder="username" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input type="password" name="password" placeholder="password" />
+            </div>
+          </div>
+        </div>
+        <div className="footer">
+          <button type="button" className="btn">
+            Login
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
