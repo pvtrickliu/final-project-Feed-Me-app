437fc9274ec3a5ab20827c8f8420a1bb1f1981db
Chuhan Jie <jiechuhan1996@gmail.com>
2019-11-25 09:45:48 -0800

    switch picture

--- routes/api/image.js
+++ routes/api/image.js
@@ -5,7 +5,10 @@ const axios = require("axios");
 const fs = require('fs');
 const appRoot = require('app-root-path');
 const shortid = require("shortid")
+const Sequelize = require("Sequelize")
+const Op = Sequelize.Op;
 
+// app.post(/api/images) // add images to database
 app.post('/', (req, res) => {
   let dotat = req.body.image_link.lastIndexOf('.')
   let ext = req.body.image_link.substr(dotat - 1);
@@ -13,8 +16,6 @@ app.post('/', (req, res) => {
   let filepath = appRoot + '/client/public/images/' + filename;
   let imagePath = 'images/' + filename;
   let url = req.body.image_link;
-  // console.log('url',url)
-  // console.log(req.body)
 
   axios({
     url,
@@ -42,83 +43,34 @@ app.post('/', (req, res) => {
   });
 });
 
-app.get('/', (req,res)=>{
-    console.log("query", req.query);
-})
 
+// app.get(/api/images?start=10&count=10) // get food type associate with img
+app.get('/', (req, res) => {
+  console.log("query", req.query);
+  let condition;
 
-  // app.post(/api/images) // add images to database
+  if (Object.keys(req.query).length === 0) {
+    res.send(res.status(400).end())
+  } else if (req.query.start && req.query.count) {
+    let start = parseInt(req.query.start) + 1;
+    let count = parseInt(req.query.start) + parseInt(req.query.count);
+    condition = {
+      where: {
+        id: {
+          [Op.between]: [start, count]
+        }
+      }
+    }
+  };
 
-  // app.get(/api/images?start=10&count=10) // get food type associate with img
+  console.log(condition)
+  db.Images.findAll(
+    condition
+  ).then(etc => {
+    res.json(etc)
+  }).catch(err => {
+    console.log(err.message)
+  });
+});
 
 module.exports = app;
-
-
-  // app.post('/', (req, res) => {
-  //   console.log(req)
-
-  //   const download_image = (url) => {
-  //     let filename = shortid.generate();
-  //     let dotat = url.lastIndexOf('.')
-  //     let ext = url.substr(dotat - 1);
-  //     let filepath = appRoot + '/client/public/images/' + filename + ext;
-  //     let imagePath = 'images/' + filename + ext;
-
-  //     axios({
-  //       url,
-  //       responseType: 'stream',
-  //     }).then(
-  //       response => {
-  //         new Promise((resolve, reject) => {
-  //           response.data
-  //             .pipe(fs.createWriteStream(filepath))
-  //             .on('finish', () => {
-  //               console.log('finish');
-  //               db.Images.create({
-  //                 image_link: imagePath,
-  //                 foodType: req.body.foodType
-  //               })
-  //                 .then(dbModel => res.json(dbModel))
-  //               resolve()
-  //             }
-  //             )
-  //             .on('error', e => reject(e));
-  //         })
-  //       }
-  //     );
-  //   }
-  //   download_image(req.body.image_link);
-  // })
-
-  
-
-
-  // findAll: function(req, res) {
-  //   db.Post.find(req.query)
-  //     .sort({ date: -1 })
-  //     .then(dbModel => res.json(dbModel))
-  //     .catch(err => res.status(422).json(err));
-  // },
-  // findById: function(req, res) {
-  //   db.Post.findById(req.params.id)
-  //     .then(dbModel => res.json(dbModel))
-  //     .catch(err => res.status(422).json(err));
-  // },
-  // create: function(req, res) {
-  //   db.Post.create(req.body)
-  //     .then(dbModel => res.json(dbModel))
-  //     .catch(err => res.status(422).json(err));
-  // },
-  // update: function(req, res) {
-  //   db.Post.findOneAndUpdate({ _id: req.params.id }, req.body)
-  //     .then(dbModel => res.json(dbModel))
-  //     .catch(err => res.status(422).json(err));
-  // },
-  // remove: function(req, res) {
-  //   db.Post.findById({ _id: req.params.id })
-  //     .then(dbModel => dbModel.remove())
-  //     .then(dbModel => res.json(dbModel))
-  //     .catch(err => res.status(422).json(err));
-  // }
-
-
